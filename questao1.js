9dc5aeb3c82dfb81e87865f5c453ab5a441786b0
Lucas Aboud <lucas_aboud@hotmail.com>
2022-07-30 15:43:18 -0300

    add resolução execicio 1 rodando em node

---
+++ questao1.js
@@ -0,0 +1,21 @@
+// 2) Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o 
+// próximo valor sempre será a soma dos 2 valores anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), 
+// escreva um programa na linguagem que desejar onde, informado um número,
+// ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o número informado pertence ou não a sequência.
+
+// IMPORTANTE:
+// Esse número pode ser informado através de qualquer entrada de sua preferência ou pode ser previamente definido no código;
+
+let entreComValor = 2584 //coloque seu valor aqui
+
+const fibonacci = (somaDosValores, valor1 = 1, valor0 = 0) => {
+    if(valor1 < somaDosValores){
+       return fibonacci(somaDosValores, valor1+valor0, valor1);
+    };
+    if(valor1 === somaDosValores){
+       return true;
+    }
+    return false;
+ };
+ console.log(fibonacci(entreComValor));
+ 
